bd8964e1fcd6e24b6f0d89d62b381f55e1c352fd
It21217654 <IT21217654@my.sliit.lk>
2024-02-06 23:20:17 +0530

    FormBuilder service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import {FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  registrationForm = new FormGroup({
-    username: new FormControl('Rash'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''), 
-    address: new FormGroup({
-      city: new FormControl(''), 
-      state: new FormControl(''), 
-      postalCode: new FormControl('')
+
+  constructor(private fb: FormBuilder){}
+  registrationForm = this.fb.group({
+    username: ['Rash'],
+    password: [''],
+    confirmPassword: [''], 
+    address: this.fb.group({
+      city: [''], 
+      state: [''], 
+      postalCode: ['']
     })
   });
+
+  // registrationForm = new FormGroup({
+  //   username: new FormControl('Rash'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''), 
+  //   address: new FormGroup({
+  //     city: new FormControl(''), 
+  //     state: new FormControl(''), 
+  //     postalCode: new FormControl('')
+  //   })
+  // });
 
   loadApiData(){
     this.registrationForm.setValue({
